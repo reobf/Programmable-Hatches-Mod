9205a4c9236947c563a2aa157b71d21c97743093
reobf <2215595288@qq.com>
2024-05-31 00:21:14 +0800

    PatternDualInputHatch is viewable in Interface Terminal now

--- src/main/java/reobf/proghatches/main/MyMod.java
+++ src/main/java/reobf/proghatches/main/MyMod.java
@@ -78,6 +78,7 @@ import reobf.proghatches.eucrafting.AECover.IMemoryCardSensitive;
 import reobf.proghatches.eucrafting.InterfaceData;
 import reobf.proghatches.eucrafting.PartEUP2PInterface;
 import reobf.proghatches.eucrafting.TileFluidInterface_EU;
+import reobf.proghatches.gt.metatileentity.PatternDualInputHatch;
 import reobf.proghatches.lang.LangManager;
 import reobf.proghatches.net.OpenPartGuiMessage;
 import reobf.proghatches.net.PriorityMessage;
@@ -210,7 +211,7 @@ public class MyMod {
 		InterfaceTerminalRegistry.instance().register(PartEUP2PInterface.class);
 		InterfaceTerminalRegistry.instance().register(PartFluidP2PInterface.class);
 		InterfaceTerminalRegistry.instance().register(TileFluidInterface_EU.class);
-
+		InterfaceTerminalRegistry.instance().register(PatternDualInputHatch.class);
 	}
 
 	@SubscribeEvent(priority = EventPriority.HIGH, receiveCanceled = false)

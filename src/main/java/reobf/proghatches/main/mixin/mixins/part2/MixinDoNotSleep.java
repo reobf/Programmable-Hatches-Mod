3a3bdfd81a78c879a05d26626be7f6cf26f556a4
unknown <zyf051520@foxmail.com>
2026-06-25 21:47:43 +0800

    update

--- src/main/java/reobf/proghatches/main/mixin/mixins/part2/MixinDoNotSleep.java
+++ src/main/java/reobf/proghatches/main/mixin/mixins/part2/MixinDoNotSleep.java
@@ -1,18 +1,23 @@
 package reobf.proghatches.main.mixin.mixins.part2;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import com.github.vfyjxf.nee.block.tile.TilePatternInterface;
+
+import appeng.helpers.DualityInterface;
 import gregtech.api.metatileentity.CommonBaseMetaTileEntity;
 import reobf.proghatches.gt.metatileentity.util.IDoNotSleep;
 
+@Mixin(value = { DualityInterface.class, TilePatternInterface.class }, remap = false)
 public abstract class MixinDoNotSleep extends CommonBaseMetaTileEntity{
 
 	
 	@Inject(method="tryDisableTicking", at = { @At("HEAD") },cancellable = true)
-    public void tryDisableTicking(CallbackInfo v) {
+    public void tryDisableTicking0(CallbackInfo v) {
 		if(this.getMetaTileEntity() instanceof IDoNotSleep y&&y.really())
 v.cancel();
     }
